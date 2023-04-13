23fcd95031df1156d9b30dbe9f5d0e0235a24614
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-13 14:45:18 -0600

    Usando el uiContext en la aplicacion

--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -17,13 +17,9 @@ export const UIProvider: FC<Props> = ({ children }) => {
 
   const [state, dispatch] = useReducer( uiReducer, UI_INITIAL_STATE )
 
-  const openSideMenu = () => {
-    dispatch({type: 'UI - Open SideBar'})
-  }
-
-  const closeSideMenu = () => {
-    dispatch({ type: 'UI - Close SideBar' })
-  }
+  const openSideMenu = () => dispatch({type: 'UI - Open SideBar'})
+  
+  const closeSideMenu = () => dispatch({ type: 'UI - Close SideBar' })
 
   return (
     <UIContext.Provider value={{
