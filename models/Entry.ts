52a6a24b1400de78883c3a79ea35c956265155c3
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-18 00:00:24 -0600

    Creando modelos de mongoose

---
+++ models/Entry.ts
@@ -0,0 +1,22 @@
+import { Entry } from '</interfaces>';
+import mongoose, { Model, Schema } from 'mongoose'
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, require: true},
+  createdAt: { type: Number},
+  status: {
+    type: String,
+    enum: {
+      values : ['pending', 'in-progress', 'finished'],
+      message: '{VALUE} no es un estado permitido'
+    }
+  }
+});
+
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', entrySchema);
+
+
+export default EntryModel;
